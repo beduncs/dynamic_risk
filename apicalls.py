bfb1e6232b477321e9c05d93a5899f8acde23817
Ben Duncan <benjamin.h.duncan@gmail.com>
2022-12-29 15:57:53 -0500

    create api app

--- apicalls.py
+++ apicalls.py
@@ -1,20 +1,36 @@
 import requests
+import logging
+import json
+from pathlib import Path
 
-#Specify a URL that resolves to your workspace
-URL = "http://127.0.0.1/"
+logging.basicConfig(level=logging.DEBUG)
+
+# Load config.json and get environment variables
+with open('config.json', 'r') as f:
+    config = json.load(f)
+
+test_data_path = Path(config['test_data_path']) / 'testdata.csv'
+api_return_path = Path(config['output_model_path']) / 'apireturns.txt'
+
+# Specify a URL that resolves to your workspace
+URL = "http://127.0.0.1"
+PORT = 8000
 
 
+# Call each API endpoint and store the responses
+logging.info("Making requests to each end point.")
 
-#Call each API endpoint and store the responses
-response1 = #put an API call here
-response2 = #put an API call here
-response3 = #put an API call here
-response4 = #put an API call here
+response1 = requests.post(
+    f'{URL}:{PORT}/prediction?filename={test_data_path}').content
+response2 = requests.get(f'{URL}:{PORT}/scoring').content
+response3 = requests.get(f'{URL}:{PORT}/summarystats').content
+response4 = requests.get(f'{URL}:{PORT}/diagnostics').content
 
-#combine all API responses
-responses = #combine reponses here
+# combine all API responses
+responses = [response1, response2, response3, response4]
+logging.debug(f"Respones: {responses}")
 
-#write the responses to your workspace
-
-
-
+# write the responses to your workspace
+logging.info(f"Writing api return respones to: {api_return_path}")
+with open(api_return_path, 'w') as api_output:
+    api_output.write(f'{responses}\n')
--- app.py
+++ app.py
@@ -1,50 +1,96 @@
-from flask import Flask, session, jsonify, request
+from flask import Flask, session, jsonify, request, make_response
 import pandas as pd
 import numpy as np
 import pickle
-import create_prediction_model
-import diagnosis 
-import predict_exited_from_saved_model
 import json
 import os
+import logging
+
+from pathlib import Path
+from diagnostics import model_predictions, dataframe_summary, dataframe_missing, execution_time, outdated_packages_list
+from scoring import score_model
 
 
+logging.basicConfig(level=logging.DEBUG)
 
-######################Set up variables for use in our script
+# Set up variables for use in our script
 app = Flask(__name__)
 app.secret_key = '1652d576-484a-49fd-913a-6879acfa6ba4'
 
-with open('config.json','r') as f:
-    config = json.load(f) 
+with open('config.json', 'r') as f:
+    config = json.load(f)
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
+dataset_csv_path = Path(config['output_folder_path']) / 'finaldata.csv'
 
 prediction_model = None
 
 
-#######################Prediction Endpoint
-@app.route("/prediction", methods=['POST','OPTIONS'])
-def predict():        
-    #call the prediction function you created in Step 3
-    return #add return value for prediction outputs
+# Prediction Endpoint
+@app.route("/prediction", methods=['POST', 'OPTIONS'])
+def predict():
+    # Get data path from user
+    filename = request.args.get('filename')
+    filename = Path(filename)
 
-#######################Scoring Endpoint
-@app.route("/scoring", methods=['GET','OPTIONS'])
-def stats():        
-    #check the score of the deployed model
-    return #add return value (a single F1 score number)
+    logging.info(f"Reading data from: {filename}")
+    # Check if the provided path exists
+    if filename.exists():
+        data_df = pd.read_csv(filename)
+        logging.debug(f"Data head: {data_df.head()}")
+    else:
+        raise ValueError("Empty data provided.")
 
-#######################Summary Statistics Endpoint
-@app.route("/summarystats", methods=['GET','OPTIONS'])
-def stats():        
-    #check means, medians, and modes for each column
-    return #return a list of all calculated summary statistics
+    # Infer with model
+    predictions = model_predictions(data_df)
 
-#######################Diagnostics Endpoint
-@app.route("/diagnostics", methods=['GET','OPTIONS'])
-def stats():        
-    #check timing and percent NA values
-    return #add return value for all diagnostics
+    return make_response(jsonify(predictions), 200)
 
-if __name__ == "__main__":    
+# Scoring Endpoint
+
+
+@app.route("/scoring", methods=['GET', 'OPTIONS'])
+def score():
+    # check the score of the deployed model
+    f1score = score_model()
+
+    return make_response(jsonify(f1score), 200)
+
+# Summary Statistics Endpoint
+
+
+@app.route("/summarystats", methods=['GET', 'OPTIONS'])
+def sumstats():
+    # Read data for test
+    logging.info(f'Reading data from: {dataset_csv_path}')
+    data_df = pd.read_csv(dataset_csv_path)
+    logging.debug(f'DataFrame head: {data_df.head()}')
+
+    # check means, medians, and modes for each column
+    stats_list = dataframe_summary(data_df)
+
+    return make_response(jsonify(stats_list), 200)
+
+# Diagnostics Endpoint
+
+
+@app.route("/diagnostics", methods=['GET', 'OPTIONS'])
+def diagnose():
+    # Read data for test
+    logging.info(f'Reading data from: {dataset_csv_path}')
+    data_df = pd.read_csv(dataset_csv_path)
+    logging.debug(f'DataFrame head: {data_df.head()}')
+
+    # Calculate missing percent per column
+    miss_percents = dataframe_missing(data_df)
+
+    # Calculate execution time
+    timings = execution_time()
+
+    # Find outdated packages
+    outdated_packages = outdated_packages_list()
+
+    return make_response(jsonify([miss_percents, timings, outdated_packages]), 200)
+
+
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000, debug=True, threaded=True)
